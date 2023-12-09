23a2d0fb83aeb644bacd8a9cc1bb89965a2c39bc
Ivy070109 <Ivyaguilar07@gmail.com>
2023-12-09 19:02:35 -0300

    borrado de comentarios innecesarios

--- src/dao/models/carts.model.js
+++ src/dao/models/carts.model.js
@@ -7,32 +7,6 @@ mongoose.pluralize(null)
 const collection = "carts"
 
 const schema = new mongoose.Schema({
-    // // products: [
-    // //     {
-    // //         product: { type: [ mongoose.Schema.Types.ObjectId ], ref: 'products' },
-    // //         quantity: Number, 
-    // //         _id: false, 
-    // //     }
-    // // ]
-    // products: [ { product: { type: [ mongoose.Schema.Types.ObjectId ], ref: 'products' }},
-    // quantity: { type: Number, _id: false },]
-
-    // products: [
-    //         {
-    //             product: {
-    //                 type: [ mongoose.Schema.Types.ObjectId ], 
-    //                 ref: 'products',
-    //                 quantity: Number,
-    //                 _id: false,    
-    //             },
-    //         },
-    //     ],
-
-    // products: { type: [ mongoose.Schema.Types.ObjectId ], ref: 'products' }, // ref a la colección
-    // //el ref quiere decir que éste array (products) tiene/guarda una referencia con la colección de products (a la que corresponde)
-    // quantity: { type: Number, required: true }
-
-
     products: {
         type: [
             {
@@ -49,13 +23,6 @@ const schema = new mongoose.Schema({
     
 })
 
-//populate
-// schema.pre('find', function() {
-//     this.populate({ path: 'products', model: productsModel });
-// })
-// schema.pre('find', function () {
-//     this.populate('products.productId')
-// })
 schema.pre('find', function(){
     this.populate('products.product')
 })
